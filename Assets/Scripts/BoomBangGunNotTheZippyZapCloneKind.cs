e451c60f35cb2fad479519734d11cfb9607b3529
Vincent Miller <vmiller0725@gmail.com>
2016-11-05 01:37:23 -0400

    Shooty bang gun

---
+++ Assets/Scripts/BoomBangGunNotTheZippyZapCloneKind.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomBangGunNotTheZippyZapCloneKind : Tool {
+    
+	// Update is called once per frame
+	void Update () {
+        if (input.GetPressDown((SteamVR_Controller.ButtonMask.Trigger))) {
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+                if (hit.collider.CompareTag("Player")) {
+                    GameObject obj = GameObject.FindGameObjectWithTag("Respawn");
+                    if (obj) {
+                        hit.transform.root.position = obj.transform.position - Vector3.up;
+                        Destroy(obj);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PleaseAttachAColliderToMyHead.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PleaseAttachAColliderToMyHead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.GetChild(2).gameObject.AddComponent<SphereCollider>().radius = 0.2f;
+        transform.GetChild(2).tag = "Player";
+
+    }
+}
