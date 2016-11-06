b55763e1986029bf2dc526f24ff4fd1d6461429c
Sean Briggs <seanrgbriggs@gmail.com>
2016-11-06 14:19:33 -0500

    + Added Penguin Dating (see BirdsOfAFeatherTool.cs and PenguinsShouldLearnHowToLove.cs) + Added Penguin Marriage + Added Penguin Divorce + Added Penguin Child Support + Added Penguin Alcoholism

---
+++ Assets/Scripts/BirdsOfAFeatherTool.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdsOfAFeatherTool : Tool {
+
+    Rigidbody penguin;
+    bool gripping;
+
+    void Update()
+    {
+        if (input.GetPress((SteamVR_Controller.ButtonMask.Trigger)) && penguin != null)
+        {
+            if (gripping) {
+                penguin.GetComponent<PenguinsShouldLearnHowToLove>().Spheniscidomagnetism();
+            }
+
+            gripping = !gripping;            
+        }
+
+        if (gripping)
+        {
+            penguin.AddForce(transform.position - penguin.position, ForceMode.Acceleration);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (!gripping) {
+            if (col.GetComponent<PenguinsShouldLearnHowToLove>())
+            {
+                penguin = col.GetComponent<Rigidbody>();
+            }else
+            {
+                penguin = null;
+            }
+        }
+    }
---
+++ Assets/Scripts/PenguinsShouldLearnHowToLove.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PenguinsShouldLearnHowToLove : MonoBehaviour {
+
+    PenguinsShouldLearnHowToLove suitor;
+    Vector3 potentialLoveSpot;
+
+    PenguinsShouldLearnHowToLove lover;
+    Vector3 localLoveSpot;
+
+    Rigidbody rb;
+    
+
+	// Use this for initialization
+	void Start () {
+        lover = null;
+        localLoveSpot = Vector3.zero;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lover != null) {
+            rb.MovePosition(rb.position + lover.transform.TransformPoint(lover.localLoveSpot) - transform.TransformPoint(localLoveSpot));
+        }
+	}
+
+    public void Spheniscidomagnetism() {
+
+        if (potentialLoveSpot.magnitude > 0)
+        {
+            lover = suitor;
+            localLoveSpot = potentialLoveSpot;
+        }
+
+    }
+
+    public void Divorce()
+    {
+        if (lover != null && localLoveSpot != Vector3.zero)
+        {
+            PenguinsShouldLearnHowToLove ex = lover;
+            lover = null;
+            localLoveSpot = Vector3.zero;
+            ex.Divorce();
+        }
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (suitor = col.gameObject.GetComponent<PenguinsShouldLearnHowToLove>())
+        {
+            potentialLoveSpot = transform.InverseTransformPoint(col.contacts[0].point);
+        }else
+        {
+            suitor = null;
+            potentialLoveSpot = Vector3.zero;
+        }
+    }
+}
