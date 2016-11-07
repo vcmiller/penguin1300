42ab51aff7b3438f683884bc5e45c640807d1a1d
Vincent Miller <vmiller0725@gmail.com>
2016-11-07 18:50:06 -0500

    + LevelDataStructures are a thing now + Saving and Loading, probably + Regions as an abstraction for levels. + Scripts for Level Select

---
+++ Assets/Scripts/LevelDataStructures/RegionController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegionController : MonoBehaviour {
+
+   // public GameObject penguin;
+    RegionPenguin[] penguins;
+
+	// Use this for initialization
+	void Start () {
+        penguins = FindObjectsOfType<RegionPenguin>();
+        for(int i = 0; i < penguins.Length && i < Game.instance.region.levels.Length; i++) {
+            penguins[i].init(Game.instance.region.levels[i], Game.instance.region.completions[i]);
+        }     
+   	}
+
+}
---
+++ Assets/Scripts/LevelDataStructures/RegionPenguin.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RegionPenguin : MonoBehaviour {
+
+    Vector3 initPos;
+    public float maxElevation = 1;
+    public float proximity = 5;
+
+    bool initialized = false;
+    public string scene { private set; get; }
+    public bool completed { private set; get; }
+
+    public void init(string s, bool c) {
+        if (!initialized) {
+            scene = s;
+            completed = c;
+            initialized = true;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        initPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(transform.position, FindObjectOfType<PleaseAttachAColliderToMyHead>().transform.position);
+        transform.localPosition = initPos + transform.up * maxElevation * Mathf.Min(1, 1 - (distance / proximity));
+	}
+
+    void OnCollisionEnter(Collision col) {
+        if (col.transform.CompareTag("Selector")) {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
