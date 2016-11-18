557c5c3673a1846b89d7cb5df4557e73515ab8a4
seanrgbriggs <seanrgbriggs@gmail.com>
2016-11-17 20:54:03 -0500

    Added SandboxDev, a shitty sandbox that probably works.
    Uses Spawner.prefab, give it a prefab and a leash distance, and you've got infinite items.

---
+++ Assets/Scripts/Construction/Sandbox/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject toSpawn;
+	public float leashDistance = 5.0f;
+	Transform spawned;
+
+	// Use this for initialization
+	void Start () {
+		name += "(" + toSpawn.name + ")";
+		Spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, spawned.position) > leashDistance) {
+			Spawn ();
+		}
+	}
+
+	void Spawn(){
+		spawned = ( (GameObject)Instantiate (toSpawn, transform.position, Quaternion.identity) ).transform;
+	}
+}
